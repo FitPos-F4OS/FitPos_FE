b7b73bcdf18e2e9f41d22f40fa7c5faa8d4ab9f7
itroqkfwk <wjdeo3321@gmail.com>
2025-03-11 14:55:57 +0900

    test#2

--- vite.config.ts
+++ vite.config.ts
@@ -8,17 +8,17 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Vite 설정 내에서 비동기 처리는 불가능하므로
+// 조건부 플러그인 로딩을 동기적으로 변경합니다.
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
     themePlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
       ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
+          // 비동기 import를 빼고 동기적으로 `cartographer` 플러그인만 로드합니다.
+          require("@replit/vite-plugin-cartographer").cartographer(),
         ]
       : []),
   ],
